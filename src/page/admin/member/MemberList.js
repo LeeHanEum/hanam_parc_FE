d8349e330513db596acc04a3556459cdc2219dfb
LeeHanEum <LeeHanEum160924@outlook.kr>
2024-02-15 01:14:31 +0900

    ADD :사용자 권한 변경 완성

--- src/page/admin/member/MemberList.js
+++ src/page/admin/member/MemberList.js
@@ -3,6 +3,7 @@ import Topnav from "../../../components/Topnav";
 import React, {useEffect, useState} from "react";
 import {Link} from "react-router-dom";
 import {MdKeyboardArrowLeft, MdKeyboardArrowRight} from "react-icons/md";
+import Select from "react-select";
 
 export default function MemberList() {
 
@@ -11,11 +12,25 @@ export default function MemberList() {
     const [page, setPage] = useState(0);
     const [size, setSize] = useState(20);
     const [totalPages, setTotalPages] = useState(0);
+    const [isUpdate, setIsUpdate] = useState(false);
+    const [memberRole, setMemberRole] = useState("");
+    const [memberStatus, setMemberStatus] = useState("");
+    let [updateId, setUpdateId] = useState(0);
 
     useEffect(() => {
         // 페이지 로딩 시 API 호출
         fetchMembers();
     }, []);
+
+    const toggleUpdate = (id) => {
+        if (isUpdate) {
+            setIsUpdate(false);
+            setUpdateId(0);
+        } else {
+            setIsUpdate(true);
+            setUpdateId(id);
+        }
+    }
 
     const fetchMembers = async () => {
         try {
@@ -32,6 +47,53 @@ export default function MemberList() {
         }
     };
 
+    const deleteMember = async (id) => {
+        try {
+            const response = await fetch(`/member?id=${id}`, {
+                method: "DELETE",
+                headers : {}
+            });
+            if (response.ok) {
+                alert("해당 사용자가 삭제되었습니다.");
+                fetchMembers();
+            } else {
+                console.error("Error deleting member:", response.statusText);
+            }
+        } catch (error) {
+            console.error("Error deleting member:", error);
+        }
+    }
+
+    const handleUpdate = async (member, memberRole, memberStatus) => {
+        try {
+            if (memberRole === "") {
+                memberRole = member.memberRole;
+            }
+            if (memberStatus === "") {
+                memberStatus = member.memberStatus;
+            }
+            const response = await fetch(`/member/role?id=${member.id}&memberRole=${memberRole}&memberStatus=${memberStatus}`, {
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    memberRole: memberRole,
+                    memberStatus: memberStatus
+                })
+            });
+            if (response.ok) {
+                alert("사용자 정보가 수정되었습니다.");
+                fetchMembers();
+                setIsUpdate(false);
+            } else {
+                console.error("Error updating member:", response.statusText);
+            }
+        } catch (error) {
+            console.error("Error updating member:", error);
+        }
+    }
+
     return (
         <>
             <div className={`page-wrapper  ${toggle ? "toggled" : ""}`}>
@@ -46,7 +108,7 @@ export default function MemberList() {
                                 <table className="w-full text-start xs:text-xs">
                                     <thead>
                                     <tr>
-                                        <th className="px-3 py-5 text-start xs:hidden">아이디</th>
+                                        <th className="px-3 py-5 text-center xs:hidden">아이디</th>
                                         <th className="px-3 py-5 text-center">이름</th>
                                         <th className="px-3 py-5 text-center xs:hidden">성별</th>
                                         <th className="px-3 py-5 text-center ">생년월일</th>
@@ -63,18 +125,52 @@ export default function MemberList() {
                                     <tbody>
                                     {members.map((member) => (
                                         <tr className="border-t border-gray-100 dark:border-gray-700" key={member.id}>
-                                            <td className="p-3 text-start xs:hidden">{member.id}</td>
+                                            <td className="p-3 text-center xs:hidden">{member.id}</td>
                                             <td className="p-3 text-center">{member.name}</td>
                                             <td className="p-3 text-center xs:hidden">{member.gender}</td>
                                             <td className="p-3 text-center">{member.birth}</td>
                                             <td className="p-3 text-center">{member.phone}</td>
                                             <td className="p-3 text-center xs:hidden">{member.address}</td>
                                             <td className="p-3 text-center xs:hidden">{member.email}</td>
-                                            <td className="p-3 text-center xs:hidden">{member.memberRole}</td>
-                                            <td className="p-3 text-center xs:hidden">{member.memberStatus}</td>
+                                            { isUpdate && updateId === member.id ?
+                                                <>
+                                                    <td className="p-3 text-center xs:hidden">
+                                                        <Select className="w-full"
+                                                                options={[
+                                                                    { value: 'GUEST', label: 'GUEST' },
+                                                                    { value: 'USER', label: 'USER'},
+                                                                    { value: 'ADMIN', label: 'ADMIN'}
+                                                                ]}
+                                                                onChange={(e) => setMemberRole(e.value)}
+                                                                defaultValue={{ value: member.memberRole, label: member.memberRole }}
+                                                        />
+                                                    </td>
+                                                    <td className="p-3 text-center xs:hidden">
+                                                        <Select className="w-full"
+                                                                options={[
+                                                                    { value: 'ACTIVE', label: 'ACTIVE' },
+                                                                    { value: 'DORMANT', label: 'DORMANT'},
+                                                                    { value: 'BLOCKED', label: 'BLOCKED'}
+                                                                ]}
+                                                                onChange={(e) => setMemberStatus(e.value)}
+                                                                defaultValue={{ value: member.memberStatus, label: member.memberStatus }}
+                                                        />
+                                                    </td>
+                                                </>
+                                            :
+
+                                                <>
+                                                <td className="p-3 text-center xs:hidden">{member.memberRole}</td>
+                                                <td className="p-3 text-center xs:hidden">{member.memberStatus}</td>
+                                                 </>
+                                            }
                                             <td className="p-3 text-center">
-                                                <Link to="#" className="py-1 px-1 inline-block font-semibold tracking-wide border align-middle duration-500 text-sm text-center hover:bg-green-700 border-green-600 hover:border-green-700 text-green-600 hover:text-white rounded-md me-2">수정</Link>
-                                                <Link to="#" className="py-1 px-1 inline-block font-semibold tracking-wide border align-middle duration-500 text-sm text-center hover:bg-red-700 border-red-600 hover:border-red-700 text-red-600 hover:text-white rounded-md">삭제</Link>
+                                                { isUpdate && updateId === member.id ?
+                                                    <Link onClick={() => handleUpdate(member, memberRole, memberStatus)} className="py-1 px-1 inline-block font-semibold tracking-wide border align-middle duration-500 text-sm text-center hover:bg-green-700 border-green-600 hover:border-green-700 text-green-600 hover:text-white rounded-md me-2">저장</Link>
+                                                    :
+                                                    <Link onClick={() => toggleUpdate(member.id)} className="py-1 px-1 inline-block font-semibold tracking-wide border align-middle duration-500 text-sm text-center hover:bg-green-700 border-green-600 hover:border-green-700 text-green-600 hover:text-white rounded-md me-2">수정</Link>
+                                                }
+                                                <Link onClick={() => deleteMember(member.id)} className="py-1 px-1 inline-block font-semibold tracking-wide border align-middle duration-500 text-sm text-center hover:bg-red-700 border-red-600 hover:border-red-700 text-red-600 hover:text-white rounded-md">삭제</Link>
                                             </td>
                                         </tr>
                                     ))}
