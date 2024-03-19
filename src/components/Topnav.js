0b778fa92998536f3fbabee3c1d5b96b29b97c3f
LeeHanEum <LeeHanEum160924@outlook.kr>
2024-03-19 20:33:15 +0900

    MOD : topnav 리팩토링

--- src/components/Topnav.js
+++ src/components/Topnav.js
@@ -6,10 +6,8 @@ import smallLogo from '../asset/image/small_logo.png'
 import * as Icon from 'react-feather'
 import {AiOutlineSetting, AiOutlineUser, BiLockAlt, FaRegComment, IoMdLogOut, MdMailOutline} from '../asset/icon/vander'
 import 'simplebar-react/dist/simplebar.min.css';
-import {User} from "feather-icons-react";
 import AuthContext from "../auth/AuthContext";
 import wlogo from '../asset/image/wlogo.png'
-import {IoHomeOutline} from "react-icons/io5";
 import {FaHome} from "react-icons/fa";
 
 export default function Topnav({setToggle, toggle}){
@@ -48,29 +46,6 @@ export default function Topnav({setToggle, toggle}){
                         <li className="dropdown inline-block relative">
                             <div className="inline-block">
                                 <Link to="/" className="mx-6 py-1 px-3 inline-block font-semibold tracking-wide items-end border duration-500 text-base text-center bg-emerald-600 border-emerald-600 text-white rounded-md me-2"><FaHome className="inline mr-2"/>홈으로 이동</Link>
-                            </div>
-                            <div className={`dropdown-menu absolute end-0 m-0 mt-4 z-10 w-44 rounded-md overflow-hidden bg-white dark:bg-slate-900 shadow dark:shadow-gray-700 ${user ? '' : 'hidden'}`} >
-                                <ul className="py-2 text-start">
-                                    <li>
-                                        <Link to="/profile" className="flex items-center font-medium py-1 px-4 dark:text-white/70 hover:text-indigo-600 dark:hover:text-white"><AiOutlineUser className="me-2"/>Profile</Link>
-                                    </li>
-                                    <li>
-                                        <Link to="/chat" className="flex items-center font-medium py-1 px-4 dark:text-white/70 hover:text-indigo-600 dark:hover:text-white"><FaRegComment className="me-2"/>Chat</Link>
-                                    </li>
-                                    <li>
-                                        <Link to="/email" className="flex items-center font-medium py-1 px-4 dark:text-white/70 hover:text-indigo-600 dark:hover:text-white"><MdMailOutline className="me-2"/>Email</Link>
-                                    </li>
-                                    <li>
-                                        <Link to="/profile-setting" className="flex items-center font-medium py-1 px-4 dark:text-white/70 hover:text-indigo-600 dark:hover:text-white"><AiOutlineSetting className="me-2"/>Settings</Link>
-                                    </li>
-                                    <li className="border-t border-gray-100 dark:border-gray-800 my-2"></li>
-                                    <li>
-                                        <Link to="/lock-screen" className="flex items-center font-medium py-1 px-4 dark:text-white/70 hover:text-indigo-600 dark:hover:text-white"><BiLockAlt className="me-2"/>Lockscreen</Link>
-                                    </li>
-                                    <li>
-                                        <Link to="/login" className="flex items-center font-medium py-1 px-4 dark:text-white/70 hover:text-indigo-600 dark:hover:text-white"><IoMdLogOut className="me-2"/>Logout</Link>
-                                    </li>
-                                </ul>
                             </div>
                         </li>
                     </ul>
