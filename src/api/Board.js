7fb76491d289a30c6ab3919120dbba220811380e
LeeHanEum <LeeHanEum160924@outlook.kr>
2024-03-25 16:03:00 +0900

    MOD : 첨부 이미지 첨부 파일 수정 완료

--- src/api/Board.js
+++ src/api/Board.js
@@ -62,3 +62,96 @@ export const fetchBoardFile = async (id) => {
         console.error("Error fetching program:", error);
     }
 }
+
+export const fetchBoard = async (id) => {
+    try {
+        const response = await fetch(`/board?id=${id}`);
+        if (response.ok) {
+            return await response.json().then(data => data.data);
+        } else {
+            console.error("Error fetching program:", response.statusText);
+        }
+    } catch (error) {
+        console.error("Error fetching program:", error);
+    }
+}
+
+export const uploadBoardImages = async (id, formData) => {
+    try {
+        const response = await fetch(`/files/${id}`,  {
+            method: 'POST',
+            body: formData
+        });
+        if (!response.ok) {
+                alert('이미지 업로드에 실패했습니다.');
+        }
+    } catch (error) {
+        console.error("Error fetching images:", error);
+    }
+}
+
+export const uploadBoardFile = async (id, formData) => {
+    try {
+        const response = await fetch(`/files/${id}/file`,  {
+            method: 'POST',
+            body: formData
+        });
+        if (!response.ok) {
+                alert('파일 업로드에 실패했습니다.');
+        }
+    } catch (error) {
+        console.error("Error fetching images:", error);
+    }
+}
+
+export const deleteBoardImage = async (id, url) => {
+    try {
+        const response = await fetch(`/files/{id}?url=${url}`, {
+            method: 'DELETE'
+        });
+        if (!response.ok) {
+            alert('이미지 삭제에 실패했습니다.');
+        }
+    } catch (error) {
+        console.error("Error fetching images:", error);
+    }
+}
+
+export const deleteBoardFile = async (id, url) => {
+    try {
+        const response = await fetch(`/files/{id}/file?url=${url}`, {
+            method: 'DELETE'
+        });
+        if (!response.ok) {
+            alert('파일 삭제에 실패했습니다.');
+        }
+    } catch (error) {
+        console.error("Error fetching images:", error);
+    }
+}
+
+export const updateBoard = async (id, title, content, category) => {
+    try {
+        const BoardRequestDto = {
+            title: title,
+            content: content,
+            boardCategory: category.value,
+        }
+
+        const response = await fetch(`/board/update?id=${id}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(BoardRequestDto),
+        });
+
+        if (!response.ok) {
+            alert('게시글 수정에 실패했습니다.');
+        }
+
+    } catch (error) {
+        console.error('Error submitting the form:', error);
+        // Handle errors or display an error message to the user
+    }
+}
--- src/page/admin/notification/UpdateBoard.js
+++ src/page/admin/notification/UpdateBoard.js
@@ -3,29 +3,63 @@ import Topnav from "../../../components/Topnav";
 import React, {useEffect, useRef, useState} from "react";
 import Select from "react-select";
 import {Link, useLocation} from "react-router-dom";
-import {fetchBoardImage} from "../../../api/Board";
-import {FaPlus} from "react-icons/fa";
+import {
+    deleteBoardFile,
+    deleteBoardImage,
+    fetchBoardFile,
+    fetchBoardImage, updateBoard,
+    uploadBoardFile,
+    uploadBoardImages
+} from "../../../api/Board";
+import {FaFile, FaPlus} from "react-icons/fa";
 
 export default function UpdateBoard() {
 
     const [toggle, setToggle] = useState(true);
-    const [fileModal, setFileModal] = useState(false);
     const [title, setTitle] = useState('');
     const [category, setCategory] = useState('');
     const [content, setContent] = useState('');
     const [images, setImages] = useState([]); // 이미지 파일 상태 추가
+    const [boardFileUrl, setBoardFileUrl] = useState([]); // 파일 URL 상태 추가
+    const [boardFileName, setBoardFileName] = useState([]); // 파일 이름 상태 추가
 
     const location = useLocation();
     const id = location.pathname.split("/")[2];
 
-    const toggleFileModal = () => {
-        setFileModal(!fileModal);
-    }
-
     const fileInputRef = useRef(null);
 
-    const handleImageUpload = () => {
-        fileInputRef.current.click();
+    const handleImageUpload = async (e) => {
+        const files = e.target.files;
+        const newImages = [...images]; // 기존 이미지 배열 복사
+
+        for (let i = 0; i < files.length; i++) {
+            const file = files[i];
+            const formData = new FormData();
+            formData.append('image', file);
+            try {
+                const response = await uploadBoardImages(id, formData);
+                const imageUrl = response.data.url; // 업로드된 이미지의 URL
+                newImages.push(imageUrl); // 새 이미지 URL을 배열에 추가
+            } catch (error) {
+                console.error("Error uploading image:", error);
+            }
+        }
+        setImages(newImages); // 업로드된 이미지를 이미지 상태에 업데이트
+    };
+
+    const handleFileUpload = async (e) => {
+        const file = e.target.files[0];
+        const formData = new FormData();
+        formData.append('file', file);
+        try {
+            const response = await uploadBoardFile(id, formData);
+            const fileUrl = response.data.url; // 업로드된 파일의 URL
+            const fileName = response.data.name; // 업로드된 파일의 이름
+            setBoardFileUrl([...boardFileUrl, fileUrl]); // 파일 URL 상태 업데이트
+            setBoardFileName([...boardFileName, fileName]); // 파일 이름 상태 업데이트
+        } catch (error) {
+            console.error("Error uploading file:", error);
+        }
     };
 
     const categoryOptions = [
@@ -36,9 +70,19 @@ export default function UpdateBoard() {
 
     useEffect(() => {
         fetchBoard();
+    }, []);
+
+    useEffect(() => {
         fetchBoardImage(id)
             .then(data => setImages(data));
-    }, []);
+    }, [images]);
+
+    useEffect(() => {
+        fetchBoardFile(id).then((data) => {
+            setBoardFileUrl(data.boardFileUrl);
+            setBoardFileName(data.boardFileName);
+        });
+    }, [boardFileUrl, boardFileName]);
 
     const getCategoryLabel = (value) => {
         switch (value) {
@@ -111,10 +155,16 @@ export default function UpdateBoard() {
                                     </div>
 
                                     <div className="mb-4 flex flex-wrap">
+                                        <div className="w-full mb-4">
+                                            <label className="font-semibold" htmlFor="images">
+                                                첨부 이미지 :
+                                            </label>
+                                        </div>
                                         {images.map((url, index) => (
                                             <div className="inline-flex relative mb-4 mr-4" key={index}>
                                                 <img key={index} src={url} alt="image" className="w-40 h-40 mr-2" />
                                                 <Link
+                                                    onClick={() => {deleteBoardImage(id, url); setImages(images.filter(image => image !== url));}}
                                                     className="w-40 absolute bottom-0 py-0.5 inline-block font-semibold tracking-wide border align-middle duration-500 text-sm text-center hover:bg-green-700 border-green-600 hover:border-green-700 text-green-600 hover:text-white rounded-md me-2"
                                                 >삭제
                                                 </Link>
@@ -125,35 +175,52 @@ export default function UpdateBoard() {
                                             <input
                                                 type="file"
                                                 accept="image/*"
-                                                className="absolute inset-0 opacity-0 w-full h-full cursor-pointer"
-                                                ref={fileInputRef}
                                                 onChange={handleImageUpload}
+                                                className="absolute inset-0 opacity-0 w-full h-full cursor-pointer"
                                             />
-                                            <div className="rounded-md w-full h-full flex justify-center items-center cursor-pointer" onClick={handleImageUpload}>
+                                            <div className="rounded-md w-full h-full flex justify-center items-center cursor-pointer">
                                                 <FaPlus className="text-6xl text-slate-400 group-hover:text-emerald-600" />
                                             </div>
                                         </div>
                                     </div>
 
-                                    <div className="mb-4 mt-4">
-                                        <button onClick={toggleFileModal} className="w-full py-2 inline-block font-semibold tracking-wide border align-middle duration-500 text-base text-center rounded-md me-2">
-                                            {fileModal ? "취소" : "첨부 파일 변경 (기존 파일 삭제됨)"}
-                                        </button>
+                                    <div className="mb-4 flex flex-wrap">
+                                        <div className="w-full mb-4">
+                                            <label className="font-semibold" htmlFor="attachments">
+                                                첨부 파일 :
+                                            </label>
+                                        </div>
+                                        {boardFileUrl.map((fileUrl, index) => (
+                                            <div className="relative flex items-center w-full mb-2 p-1" key={index}>
+                                                <a href={`${process.env.PUBLIC_URL}${fileUrl}`} download className="text-blue-500 hover:underline mr-2">
+                                                    <FaFile className="text-lg mr-2 inline" />
+                                                    {boardFileName[index]}
+                                                </a>
+                                                <Link
+                                                    onClick={() => {deleteBoardFile(id, fileUrl); setBoardFileUrl(boardFileUrl.filter(url => url !== fileUrl)); setBoardFileName(boardFileName.filter(name => name !== boardFileName[index]));}}
+                                                    className="px-1 py-0.5 inline-block font-semibold tracking-wide border align-middle duration-500 text-sm text-center hover:bg-green-700 border-green-600 hover:border-green-700 text-green-600 hover:text-white rounded-md me-2"
+                                                >삭제
+                                                </Link>
+                                            </div>
+                                        ))}
+                                        <div className="w-full relative">
+                                            <input
+                                                type="file"
+                                                className="hidden"
+                                                ref={fileInputRef}
+                                                onChange={handleFileUpload}
+                                            />
+                                            <button
+                                                className="focus:outline-none text-emerald-600"
+                                                onClick={() => fileInputRef.current.click()}
+                                            >
+                                                <FaPlus className="text-lg mr-2 inline" />
+                                                파일 추가하기
+                                            </button>
+                                        </div>
                                     </div>
 
-                                    {fileModal && (
-                                    <div className="mb-4">
-                                        <label className="font-semibold block" htmlFor="file">
-                                            파일 첨부 :
-                                        </label>
-                                        <input
-                                            id="file"
-                                            type="file"
-                                            multiple
-                                            className="form-input mt-3 w-[400px] py-2 px-3 h-10 bg-transparent dark:bg-slate-900 dark:text-slate-200 rounded outline-none border border-gray-200 focus:border-indigo-600 dark:border-gray-800 dark:focus:border-indigo-600 focus:ring-0"
-                                        />
-                                    </div>
-                                    )}
+
                                 </div>
 
                             </div>
@@ -166,7 +233,7 @@ export default function UpdateBoard() {
                                         id="content"
                                         value={content}
                                         onChange={(e) => setContent(e.target.value)}
-                                        className="form-input mt-3 w-full py-2 px-3 h-87 bg-transparent dark:bg-slate-900 dark:text-slate-200 rounded outline-none border border-gray-200 focus:border-indigo-600 dark:border-gray-800 dark:focus:border-indigo-600 focus:ring-0"
+                                        className="form-input mt-3 w-full py-2 px-3 h-[400px] bg-transparent dark:bg-slate-900 dark:text-slate-200 rounded outline-none border border-gray-200 focus:border-indigo-600 dark:border-gray-800 dark:focus:border-indigo-600 focus:ring-0"
                                         placeholder="내용을 입력하세요"
                                         style={{resize: "none"}}
                                         wrap="hard"
@@ -175,9 +242,13 @@ export default function UpdateBoard() {
 
                                 <div className="mt-8">
                                     <Link
+                                        onClick={() => {
+                                            updateBoard(id, title, content, category)
+                                                .then(() => alert('게시글이 수정되었습니다.'));
+                                        }}
                                         className="w-full py-2 inline-block font-semibold tracking-wide border align-middle duration-500 text-base text-center hover:bg-green-700 border-green-600 hover:border-green-700 text-green-600 hover:text-white rounded-md me-2"
                                     >
-                                        글 등록하기
+                                        글 수정 완료하기
                                     </Link>
                                 </div>
 
