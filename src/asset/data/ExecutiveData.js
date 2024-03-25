613dcb5a475090188ecb5e372c32374f07ae832a
LeeHanEum <LeeHanEum160924@outlook.kr>
2024-03-25 16:26:38 +0900

    MOD : 임원 추가

--- src/asset/data/ExecutiveData.js
+++ src/asset/data/ExecutiveData.js
@@ -1,176 +1,194 @@
 export const executives = [
     {
-        id:1,
-        spot : "회장",
-        name : "이현재",
-        belong : "하남시장"
+        id: 1,
+        spot: "회장",
+        name: "이현재",
+        belong: "하남시장"
     },
     {
-        id : 2,
-        spot : "수석부회장",
-        name : "유연석",
-        belong : "유경건설㈜ 회장"
+        id: 2,
+        spot: "수석부회장",
+        name: "유연석",
+        belong: "유경건설㈜ 회장"
     },
     {
-        id : 3,
-        spot : "고문",
-        name : "문양수",
-        belong : "㈜금호T&C 대표"
+        id: 3,
+        spot: "고문",
+        name: "문양수",
+        belong: "㈜금호T&C 대표"
     },
     {
-        id : 4,
-        spot : "고문",
-        name : "김영기",
-        belong : "한채당 대표"
+        id: 4,
+        spot: "고문",
+        name: "김영기",
+        belong: "한채당 대표"
     },
     {
-        id : 5,
-        spot : "부회장",
-        name : "한동윤",
-        belong : "하남시장애인연합회 회장"
+        id: 5,
+        spot: "부회장",
+        name: "한동윤",
+        belong: "하남시장애인연합회 회장"
     },
     {
-        id : 6,
-        spot : "부회장",
-        name : "김동한",
-        belong : "창호공장 대표"
+        id: 6,
+        spot: "부회장",
+        name: "김동한",
+        belong: "창호공장 대표"
     },
     {
-        id : 7,
-        spot : "부회장",
-        name : "신동국",
-        belong : "명성건설 대표"
+        id: 7,
+        spot: "부회장",
+        name: "신동국",
+        belong: "명성건설 대표"
     },
     {
-        id : 8,
-        spot : "부회장",
-        name : "김응경",
-        belong : "농협하남시 지부장"
+        id: 8,
+        spot: "부회장",
+        name: "김응경",
+        belong: "농협하남시 지부장"
     },
     {
-        id : 9,
-        spot : "부회장",
-        name : "이구운",
-        belong : "KB국민은행 하남지역 본부장"
+        id: 9,
+        spot: "부회장",
+        name: "이재홍",
+        belong: "KB국민은행 하남지역 본부장"
     },
     {
-        id : 10,
-        spot : "부회장",
-        name : "이희근",
-        belong : "㈜황산디벨럽먼트 대표"
+        id: 10,
+        spot: "부회장",
+        name: "이희근",
+        belong: "㈜황산디벨럽먼트 대표"
     },
     {
-        id : 11,
-        spot : "부회장",
-        name : "정현범",
-        belong : "농협은행 서울영업본부장"
+        id: 11,
+        spot: "부회장",
+        name: "정현범",
+        belong: "농협은행 서울영업본부장"
     },
     {
-        id : 12,
-        spot : "상임이사",
-        name : "권정국",
-        belong : "스마트로지스 대표"
+        id: 12,
+        spot: "상임이사",
+        name: "권정국",
+        belong: "스마트로지스 대표"
     },
     {
-        id : 13,
-        spot : "상임이사",
-        name : "박화규",
-        belong : "일강건설 전무이사"
+        id: 13,
+        spot: "상임이사",
+        name: "박화규",
+        belong: "일강건설 전무이사"
     },
     {
-        id : 14,
-        spot : "상임이사",
-        name : "임성기",
-        belong : "네파 검단산점 대표"
+        id: 14,
+        spot: "상임이사",
+        name: "임성기",
+        belong: "네파 검단산점 대표"
     },
     {
-        id : 15,
-        spot : "상임이사",
-        name : "문진한",
-        belong : "동화건설주차빌딩 대표"
+        id: 15,
+        spot: "상임이사",
+        name: "문진한",
+        belong: "동화건설주차빌딩 대표"
     },
     {
-        id : 16,
-        spot : "상임이사",
-        name : "안성진",
-        belong : "거성창호 대표"
+        id: 16,
+        spot: "상임이사",
+        name: "안성진",
+        belong: "거성창호 대표"
     },
     {
-        id : 17,
-        spot : "이사",
-        name : "최상호",
-        belong : "경기도장애인복지회 하남지부장"
+        id: 17,
+        spot: "상임이사",
+        name: "한준수",
+        belong: "(주)우림피엠씨 대표"
     },
     {
-        id : 18,
-        spot : "이사",
-        name : "정숙자",
-        belong : "장애인자립센터 소장"
+        id: 18,
+        spot: "상임이사",
+        name: "신영자",
+        belong: "(주)씨엔 대표"
     },
     {
-        id : 19,
-        spot : "이사",
-        name : "남상헌",
-        belong : "한국농아인협회 하남지회장"
+        id: 19,
+        spot: "이사",
+        name: "최상호",
+        belong: "경기도장애인복지회 하남지부장"
     },
     {
-        id : 20,
-        spot : "이사",
-        name : "이희수",
-        belong : "前)하남시장애인부모회 회장"
+        id: 20,
+        spot: "이사",
+        name: "정숙자",
+        belong: "장애인자립센터 소장"
     },
     {
-        id : 21,
-        spot : "이사",
-        name : "유영자",
-        belong : "경기도시각장애인연합회 하남지회장"
+        id: 21,
+        spot: "이사",
+        name: "남상헌",
+        belong: "한국농아인협회 하남지회장"
     },
     {
-        id : 22,
-        spot : "이사",
-        name : "김동진",
-        belong : "㈜도명디앤이 대표"
+        id: 22,
+        spot: "이사",
+        name: "이희수",
+        belong: "前)하남시장애인부모회 회장"
     },
     {
-        id : 23,
-        spot : "이사",
-        name : "김경학",
-        belong : "성광학교 교장"
+        id: 23,
+        spot: "이사",
+        name: "유영자",
+        belong: "경기도시각장애인연합회 하남지회장"
     },
     {
-        id : 24,
-        spot : "이사",
-        name : "공상철",
-        belong : "공가네국밥 대표"
+        id: 24,
+        spot: "이사",
+        name: "김동진",
+        belong: "㈜도명디앤이 대표"
     },
     {
-        id : 25,
-        spot : "이사",
-        name : "이경훈",
-        belong : "㈜쌈바 대표"
+        id: 25,
+        spot: "이사",
+        name: "김경학",
+        belong: "성광학교 교장"
     },
     {
-        id : 26,
-        spot : "이사",
-        name : "이원진",
-        belong : "동체육회 연합회장"
+        id: 26,
+        spot: "이사",
+        name: "공상철",
+        belong: "공가네국밥 대표"
     },
     {
-        id : 27,
-        spot : "이사",
-        name : "천성욱",
-        belong : "㈜천성정밀 대표"
+        id: 27,
+        spot: "이사",
+        name: "이경훈",
+        belong: "㈜쌈바 대표"
     },
     {
-        id : 28,
-        spot : "감사",
-        name : "민복기",
-        belong : "하남시장애인복지관장"
+        id: 28,
+        spot: "이사",
+        name: "이원진",
+        belong: "동체육회 연합회장"
     },
     {
         id: 29,
+        spot: "이사",
+        name: "천성욱",
+        belong: "㈜천성정밀 대표"
+    },
+    {
+        id: 30,
+        spot: "이사",
+        name: "최지현",
+        belong: "플라워 현 대표"
+    },
+    {
+        id: 31,
+        spot: "감사",
+        name: "민복기",
+        belong: "하남시장애인복지관장"
+    },
+    {
+        id: 32,
         spot: "감사",
         name: "이진희",
         belong: "前)성광학교 장"
-    }
-]
+    },
+];
